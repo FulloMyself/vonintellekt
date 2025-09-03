49046802dcd533e7eab45a4c675afab9100f5187
FullofMyself <wmaseko1@live.com>
2025-09-03 03:33:00 +0200

    Supabase

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Replace 'vonintellekt' with your GitHub repo name
 export default defineConfig({
+  base: "/vonintellekt/",
   plugins: [react()],
-})
+});
