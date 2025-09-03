ce949ddb2d58aecc319468029dbd190b5afe3fe0
FullofMyself <wmaseko1@live.com>
2025-09-03 03:27:16 +0200

    Supabase

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../SupabaseClient";
 
 export default function Profile() {
   const [user, setUser] = useState(null);
