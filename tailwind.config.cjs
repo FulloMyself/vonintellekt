ae80190946a118e247b08244a2ebbf8f9370b418
FullofMyself <wmaseko1@live.com>
2025-09-05 13:36:29 +0200

    Shop

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,3 +19,12 @@ module.exports = {
   },
   plugins: [],
 };
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
