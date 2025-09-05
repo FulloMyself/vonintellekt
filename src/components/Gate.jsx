90df869c53fd8e9d91ad05756fea8226940b7a69
FullofMyself <wmaseko1@live.com>
2025-09-05 10:05:16 +0200

    TVScreen

--- src/components/Gate.jsx
+++ src/components/Gate.jsx
@@ -4,7 +4,7 @@ import "./Gate.css";
 const Gate = ({ onEnter }) => (
   <div className="gate-container">
     <div className="gate-content">
-      <h1 className="gate-logo"> </h1>
+      <h1 className="gate-logo">Von Intellekt</h1>
       <button className="gate-enter-btn" onClick={onEnter}>
         Press Start
       </button>
